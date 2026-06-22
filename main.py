17a8d0b1182ee82652c5655cf2969072a15ffaf1
Neha Sirohi <T4291@novel.com>
2026-06-22 12:16:11 +0530

    working

--- main.py
+++ main.py
@@ -1,33 +1,124 @@
-def calculate(hand):
-    total=0
-    aces=0
+
+import random
+
+Rank = ["A",2,3,4,5,6,7,8,9,10,"J","Q","K"]
+suits = ["Heart","Diamond","Clubs","Spades"] 
+
+
+def create_deck(): 
+    deck = []
+
+    for i in suits:
+        for j in Rank:
+            deck.append(f"{j}{i[0]}")
+
+    random.shuffle(deck)
+    return deck 
+
+
+
+def calculate(hands):
+    total = 0
+    aces = 0
+    for card in hands:
+        rank = card[:-1]
+
+        if rank in ["J","K","Q"]:
+            total += 10
     
-    for card in hand:
-        if card in ['K','J','Q']:
-            total = total + 10
-        elif card =='A':
+        elif rank == "A":
+            total += 11
             aces += 1
-            total +=11
+    
+        else:
+            total += int(rank)
+        
+    while total > 21 and  aces > 0:
+            total -= 10
+            aces -= 1
+    
+
+
+    return total
+
+
+def player_turn(player_hand,deck):
+     
+    while True:
+        print("\nPlayer Hand:", player_hand)
+        print("Player Score:", calculate(player_hand))
+
+        choice = input("Hit or Stand? ").lower()
+
+        if choice == "hit":
+            player_hand.append(deck.pop())
+
+        if calculate(player_hand) > 21:
+            print("\nPlayer Hand:", player_hand)
+            print("Player Score:", calculate(player_hand))
+            print("Bust! Dealer wins.")
+            return False
+
+        elif choice == "stand":
+            return True
 
         else:
-            total = total + card
-    while total > 21 and aces > 0:
-        total-=10
-        aces-=1
-    return total
-def blackjack(hand_1,hand_2):
-    total_1 = calculate(hand_1)
-    total_2 = calculate(hand_2)
-    if total_1> 21:
-        print("hand_2 wins")
-    elif total_2> 21:
-        print("hand_1 wins")
-    elif total_1>total_2:
-        print("hand_1 wins")
-    elif total_2>total_1:
-        print("hand_2 wins")
+            print("Enter hit or stand.")
+
+def dealer_turn(dealer_hand,deck):
+
+    while calculate(dealer_hand)< 17:
+         dealer_hand.append(deck.pop())
+
+
+def determine_winner(player_hand,dealer_hand):
+
+    player_score = calculate(player_hand)
+    dealer_score = calculate(dealer_hand)
+
+    print("\nFinal Hands")
+    print("palyer:",player_hand)
+    print("player score:",player_score)
+
+    print("dealer:",dealer_hand)
+    print("dealer score:",dealer_score) 
+
+    if player_score > 21:
+        print("player busts! dealer wins.")
+
+    elif dealer_score > 21:
+        print("dealer busts! player wins.")
+
+    elif player_score > dealer_score:
+        print("player wins.")
+
+    elif dealer_score > player_score:
+        print("dealer wins.")
+
     else:
-        print("tie")
-blackjack([1,'K',2],['J','Q','K'])
+        print("push(tie)")
+
+def main():
+    deck = create_deck()
+
+    player_hand = []
+    dealer_hand = []
+
+    player_hand.append(deck.pop())
+    dealer_hand.append(deck.pop())
+    player_hand.append(deck.pop())
+    dealer_hand.append(deck.pop())
+
+    print("player:",player_hand)
+    print("dealer:",dealer_hand)
+
+    player_alive = player_turn(player_hand,deck)
+
+    if player_alive:
+        dealer_turn(dealer_hand,deck)
+        determine_winner(player_hand,dealer_hand)
+
+main()
+
 
 
